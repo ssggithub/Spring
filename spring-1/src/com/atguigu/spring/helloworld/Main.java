6be8b7719d92706d07eec396c237a968fb80f5a6
ssggithub <13691897490@163.com>
2017-02-22 13:35:53 +0800

    spring   构造函数注入

--- spring-1/src/com/atguigu/spring/helloworld/Main.java
+++ spring-1/src/com/atguigu/spring/helloworld/Main.java
@@ -10,6 +10,8 @@ public class Main {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 
 		// 2. 从 IOC 容器中获取 bean 的实例
+		// 根据类型来获取 bean 的实例: 要求在 IOC 容器中只有一个与之类型匹配的 bean, 若有多个则会抛出异常.
+		// 一般情况下, 该方法可用, 因为一般情况下, 在一个 IOC 容器中一个类型对应的 bean 也只有一个.
 		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
 		helloWorld.hello();
 	}
